7625982ddebbd94c1da91a0120228bc7d695f209
ykh9871 <ykh9871@gmail.com>
2024-03-07 00:47:50 +0900

    Update Footer component
    Footer 컴포넌트를 업데이트하였습니다.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
-  <footer class="text-muted py-5">
-
+  <footer class="fixed bottom-0 w-full py-1 text-center text-white bg-gray-600">
+    <p class="text-sm">&copy; MacTop 2024</p>
   </footer>
 </template>
 
