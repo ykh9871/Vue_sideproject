74f3de0a0fba80f34f18bfe0ce76cf845175b0f6
ykh9871 <ykh9871@gmail.com>
2024-03-07 00:49:57 +0900

    Update Home.vue
    Home.vue 파일을 업데이트하였습니다. 이제 MacBook Pro 및 MacBook Air 제품에 대한 섹션 디자인이 개선되었으며, 제품에 대한 간략한 설명과 제품 페이지로 이동하는 링크가 추가되었습니다.

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,38 +1,31 @@
 <template>
-  <div class="home">
-    <div class="album py-5 bg-light">
-      <div class="container">
-        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-          <div class="col" v-for="(item, idx) in state.items" :key="idx">
-            <Card :item="item"/>
-          </div>
+    <section>
+      <div class="grid grid-cols-1 gap-4 h-dvh md:grid-cols-1">
+        <div class="w-auto h-auto px-10 py-24 mb-4 text-black bg-top bg-cover md:w-auto md:h-full "
+            style="background-image: url(https://www.apple.com/v/macbook-pro/aj/images/overview/welcome/welcome_hero_endframe__66ipqm3o5gyu_large.jpg)">
+            <p class="mb-1 text-5xl font-bold">MacBook Pro</p>
+            <p class="mb-3 text-6xl font-bold">비교 불가 성능.</p>
+            <p class="mb-10 text-6xl font-bold ">시선 강탈 매력.</p>
+            <routerLink to="/products"
+                    class="px-8 py-4 text-base font-bold text-white uppercase bg-black rounded hover:bg-gray-200 hover:text-gray-800">MacBook Pro 보러가기</routerLink>
+        </div>
+        <div  class="w-auto h-auto px-10 py-24 mb-4 text-white bg-top bg-cover md:w-auto md:h-full"
+            style="background-image: url(https://www.apple.com/v/macbook-air/q/images/overview/macbook_air_m1__dxctrdvvxm4i_large.jpg)">
+            <div class="md:w-1/2">
+                <p class="mb-1 text-6xl font-bold">MacBook Air</p>
+                <p class="mb-3 text-4xl font-bold">어디서든 일하고, 즐기고, 창작할 수 있도록</p>
+                <p class="mb-10 text-4xl font-bold">해주는 놀랍도록 얇고 빠른 노트북.</p>
+                <routerLink to="/products"
+                    class="px-8 py-4 text-base font-bold text-black uppercase bg-gray-200 rounded hover:bg-gray-200 hover:text-gray-800">MacBook Air보러가기</routerLink>
+            </div>
         </div>
       </div>
-    </div>
-  </div>
+    </section>
 </template>
 
 <script>
-import axios from "axios";
-import {reactive} from "vue";
-import Card from "@/components/Card";
-
-export default {
-  name: "Home",
-  components: {Card},
-  setup() {
-    const state = reactive({
-      items: []
-    })
-
-    axios.get("/api/product/").then(({data}) => {
-      state.items = data;
-    });
-
-    return {state}
-  }
-}
 </script>
+
 
 <style scoped>
 
