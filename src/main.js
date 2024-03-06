b917000d092de82d63fb2316711936d161d95562
ykh9871 <ykh9871@gmail.com>
2024-03-07 00:45:19 +0900

    Add Tailwind CSS import statement in main.js
    main.js 파일에 Tailwind CSS를 import하는 구문을 추가하였습니다. 이로써 Tailwind CSS 스타일이 애플리케이션에 적용됩니다.

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import store from "@/scripts/store"
 import router from "@/scripts/router";
 import App from './App.vue'
+import "./assets/css/tailwind.css"
 
 createApp(App).use(store).use(router).mount('#app')
