e454cabf548ddb60e64b2900ebaf4bb1a6b4b524
ykh9871 <ykh9871@gmail.com>
2024-03-07 00:51:57 +0900

    Refactor Products page to use reactive state and Card component
    Products 페이지를 리액티브 상태와 Card 컴포넌트를 사용하도록 리팩토링하였습니다. 이제 상품 목록을 불러와 각 상품을 Card 컴포넌트로 표시합니다.

---
+++ src/pages/Products.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="home">
+    <div class="py-5 bg-gray-100">
+      <div class="container mx-auto">
+        <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
+          <div v-for="(item, idx) in state.items" :key="idx">
+            <Card :item="item"/>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import {reactive} from "vue";
+import Card from "@/components/Card";
+
+export default {
+  name: "Products",
+  components: {Card},
+  setup() {
+    const state = reactive({
+      items: []
+    })
+
+    axios.get("/api/product/").then(({data}) => {
+      state.items = data;
+    });
+
+    return {state}
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
