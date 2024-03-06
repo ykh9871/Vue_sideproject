0c0a5d228871e3752eb5f502984cdf88cca090bf
ykh9871 <ykh9871@gmail.com>
2024-03-07 00:49:10 +0900

    Update login/logout functionality in Header.vue
    Header.vue 파일에서 로그인 및 로그아웃 관련 기능을 업데이트하였습니다. 이제 모바일 환경에서 메뉴를 열고 닫을 수 있도록 토글 기능이 추가되었으며, 로그아웃 시에는 사용자 상태를 초기화하고 홈페이지로 이동하는 동작이 수행됩니다.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,44 +1,82 @@
 <template>
-  <header>
-    <div class="collapse bg-dark" id="navbarHeader">
-      <div class="container">
-        <div class="row">
-          <div class="col-sm-4 py-4">
-            <h4 class="text-white">사이트맵</h4>
-            <ul class="list-unstyled">
-              <li>
-                <router-link to="/" class="text-white">메인 화면</router-link>
-              </li>
-              <li v-if="$store.state.account.id">
-                <router-link to="/orders" class="text-white">주문 내역</router-link>
-              </li>
-              <li>
-                <router-link to="/login" class="text-white" v-if="!$store.state.account.id">로그인</router-link>
-                <a to="/login" class="text-white" @click="logout()" v-else>로그아웃</a>
-              </li>
-            </ul>
-          </div>
-        </div>
+  <header class="h-auto bg-gray-200">
+    <nav class="flex items-center justify-between h-16 max-w-full p-3 mx-auto lg:px-8" aria-label="Global">
+      <div class="flex lg:flex-1">
+        <router-link to="/">
+         <img src="@/assets/mactop.png" class="w-auto h-10"/>
+        </router-link>
       </div>
-    </div>
-    <div class="navbar navbar-dark bg-dark shadow-sm">
-      <div class="container">
-        <router-link to="/" class="navbar-brand d-flex align-items-center">
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor"
-               stroke-linecap="round" stroke-linejoin="round" stroke-width="2" aria-hidden="true" class="me-2"
-               viewBox="0 0 24 24">
-            <path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"/>
-            <circle cx="12" cy="13" r="4"/>
-          </svg>
-          <strong>Labtop</strong>
-        </router-link>
-        <router-link to="/cart" class="cart btn" v-if="$store.state.account.id">
-          <i class="fa fa-shopping-cart" aria-hidden="true"></i>
-        </router-link>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader"
-                aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+      <div class="flex lg:hidden">
+        <button @click="toggleMobileMenu" id="open-mobile-menu" type="button"
+            class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+            <span class="sr-only">Open main menu</span>
+            <svg class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
         </button>
+      </div>
+      <div class="hidden lg:flex lg:gap-x-12">
+          <div class="px-3 bg-indigo-100 rounded-md">
+              <router-link to="/" class="text-lg font-semibold leading-none text-gray-900">Main</router-link>
+          </div>
+          <div class="px-3" v-if="$store.state.account.isLogin">
+              <router-link to="/orders" class="text-lg font-semibold leading-none text-gray-900">Orders</router-link>
+          </div>
+          <div class="px-3">
+              <router-link to="/products" class="text-lg font-semibold leading-none text-gray-900">Products</router-link>
+          </div>
+          <div class="px-3" v-if="$store.state.account.isLogin">
+              <router-link to="#" class="text-lg font-semibold leading-none text-gray-900">My Page</router-link>
+          </div>
+      </div>
+      <div class="hidden px-3 py-1 login lg:flex lg:flex-1 lg:justify-end">
+          <a class="block px-3 py-4 ml-10 -mx-3 text-base font-semibold leading-7 text-gray-900 rounded-lg login hover:bg-gray-50"  v-if="!$store.state.account.isLogin"
+          @click="$router.push('/login')">Login</a>
+          <a class="block px-3 py-4 ml-10 -mx-3 text-base font-semibold leading-7 text-gray-900 rounded-lg login hover:bg-gray-50" v-else @click="logout()">Logout</a>
+      </div>
+    </nav>
+    <!-- Mobile menu, show/hide based on menu open state. -->
+    <div :class="{ 'hidden': !isMobileMenuOpen }" id="mobile-menu-container" class="lg:hidden" role="dialog" aria-modal="true">
+      <!-- Background backdrop, show/hide based on slide-over state. -->
+      <div class="fixed inset-0 z-10"></div>
+      <div
+          class="fixed inset-y-0 right-0 z-10 w-full px-6 py-6 overflow-y-auto bg-white sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div class="flex items-center justify-between">
+              <router-link to="/" class="-m-1.5 p-1.5">
+                <img src="@/assets/mactop.png" class="w-auto h-12"/>
+              </router-link>
+              <button @click="toggleMobileMenu" id="close-menu-button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
+                  <span class="sr-only">Close menu</span>
+                  <svg class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                      aria-hidden="true">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                  </svg>
+              </button>
+          </div>
+          <div class="flow-root mt-6">
+              <div class="grid grid-cols-1 gap-4 p-3 mx-auto sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 max-w-7xl">
+                  <div class="py-6 space-y-2">
+                      <div class="-mx-3">
+                          <router-link to="/" @click="toggleMobileMenu"
+                              class="block px-3 py-4 ml-10 -mx-3 text-base font-semibold leading-7 text-gray-900 rounded-lg hover:bg-gray-50">Main</router-link>
+                          <router-link to="/orders" @click="toggleMobileMenu" v-if="$store.state.account.isLogin"
+                              class="block px-3 py-4 ml-10 -mx-3 text-base font-semibold leading-7 text-gray-900 rounded-lg hover:bg-gray-50">Orders</router-link>
+                          <router-link to="/products" @click="toggleMobileMenu"
+                              class="block px-3 py-4 ml-10 -mx-3 text-base font-semibold leading-7 text-gray-900 rounded-lg hover:bg-gray-50">Products</router-link>
+                          <router-link to="#" @click="toggleMobileMenu" v-if="$store.state.account.isLogin"
+                              class="block px-3 py-4 ml-10 -mx-3 text-base font-semibold leading-7 text-gray-900 rounded-lg my-page hover:bg-gray-50">My Page</router-link>
+                          <router-link to="/login" @click="toggleMobileMenu"
+                                       class="block px-3 py-4 ml-10 -mx-3 text-base font-semibold leading-7 text-gray-900 rounded-lg login hover:bg-gray-50" 
+                                       v-if="!$store.state.account.isLogin">Login</router-link>
+                          <a 
+                             class="block px-3 py-4 ml-10 -mx-3 text-base font-semibold leading-7 text-gray-900 rounded-lg login hover:bg-gray-50" 
+                             @click="logout()" v-else>Logout</a>
+                      </div>
+                  </div>
+              </div>
+          </div>
       </div>
     </div>
   </header>
@@ -49,14 +87,25 @@ import store from "@/scripts/store";
 import router from "@/scripts/router";
 
 export default {
+  data () {
+    return {
+      isMobileMenuOpen: false // 모바일 메뉴 상태를 저장하는 변수
+    }
+  },
   name: 'Header',
+  methods: {
+    toggleMobileMenu() {
+      this.isMobileMenuOpen = !this.isMobileMenuOpen; // 모바일 메뉴 상태를 토글
+    },
+  },
   setup() {
     const logout = () => {
       // 세션 스토리지에서 JWT 토큰 삭제
-      sessionStorage.removeItem('token');
+      localStorage.removeItem('accessToken');
+      localStorage.removeItem('refreshToken');
       
       // 사용자 상태 초기화
-      store.commit('setAccount', 0);
+      store.commit('setAccount', { email: '', exp: '', isLogin: false });
       
       // 홈페이지로 이동
       router.push({ path: '/' });
@@ -67,13 +116,5 @@ export default {
 };
 </script>
 
-<style scoped>
-header ul li a {
-  cursor: pointer;
-}
-
-header .navbar .cart {
-  margin-left: auto;
-  color: #fff;
-}
+<style>
 </style>
