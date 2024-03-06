dc78e1cf4b251db40be59abd151b13884e1f05d8
ykh9871 <ykh9871@gmail.com>
2024-03-07 00:44:22 +0900

    Refactor login status check logic and remove unnecessary style in App.vue
    App.vue 파일에서 불필요한 style 부분을 제거하고 로그인 상태 확인 로직을 재구성하였습니다. 이제 로그인 상태를 확인하기 위해 로컬 스토리지에서 JWT를 가져와 검사하고, JWT가 없는 경우 사용자를 로그아웃 상태로 설정합니다.

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import store from "@/scripts/store";
 import {watch} from "vue";
-import {useRoute} from "vue-router/dist/vue-router";
+import {useRoute} from "vue-router";
 
 export default {
   name: 'App',
@@ -19,71 +19,21 @@ export default {
   },
   setup() {
   const checkToken = () => {
-    const token = sessionStorage.getItem('token'); // 세션 스토리지에서 JWT를 가져옵니다.
-    if (!token) {
+    const accessToken = localStorage.getItem('accessToken'); // 세션 스토리지에서 JWT를 가져옵니다.
+    if (!accessToken) {
       // JWT가 없으면 사용자를 로그아웃 상태로 설정합니다.
-      store.commit("setAccount", 0);
+      store.commit("setAccount", '');
     }
   };
 
   const route = useRoute();
 
-  watch(route, () => {
-    checkToken();
-  });
-}
+    watch(route, () => {
+      checkToken();
+    });
+  }
 }
 </script>
 
 <style>
-.bd-placeholder-img {
-  font-size: 1.125rem;
-  text-anchor: middle;
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  user-select: none;
-}
-
-@media (min-width: 768px) {
-  .bd-placeholder-img-lg {
-    font-size: 3.5rem;
-  }
-}
-
-.b-example-divider {
-  height: 3rem;
-  background-color: rgba(0, 0, 0, .1);
-  border: solid rgba(0, 0, 0, .15);
-  border-width: 1px 0;
-  box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
-}
-
-.b-example-vr {
-  flex-shrink: 0;
-  width: 1.5rem;
-  height: 100vh;
-}
-
-.bi {
-  vertical-align: -.125em;
-  fill: currentColor;
-}
-
-.nav-scroller {
-  position: relative;
-  z-index: 2;
-  height: 2.75rem;
-  overflow-y: hidden;
-}
-
-.nav-scroller .nav {
-  display: flex;
-  flex-wrap: nowrap;
-  padding-bottom: 1rem;
-  margin-top: -1px;
-  overflow-x: auto;
-  text-align: center;
-  white-space: nowrap;
-  -webkit-overflow-scrolling: touch;
-}
 </style>
